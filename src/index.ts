dfa3e77cea61ab61080570a6b75eb169416cb159
Luke Kasson <luke.kasson@gmail.com>
2019-10-15 19:52:14 +0000

    done with day 1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { multiply } from "./math";
+
+const num1 = document.getElementById("number1") as HTMLInputElement;
+const num2 = document.getElementById("number2") as HTMLInputElement;
+const multiplyButton = document.getElementById("multiply") as HTMLInputElement;
+const answer = document.getElementById("answer") as HTMLInputElement;
+
+
+multiplyButton.addEventListener('click', () => {
+    const n1 = num1.valueAsNumber;
+    const n2 = num2.valueAsNumber;
+
+    const ans = multiply(n1, n2);
+
+    answer.innerText = ans.toString();
+})
+
 
